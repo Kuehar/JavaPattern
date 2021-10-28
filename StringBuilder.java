4029f734a426f131093c0105151e322517095197
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-10-28 22:18:27 +0900

    Create StringBuilder.java

---
+++ StringBuilder.java
@@ -0,0 +1,7 @@
+public class StringBuilder {
+	public static void main(String[] args) {
+		String s1 = "012";
+		String s2 = s1.concat("345"); // 文字列結合
+		System.out.println(s2);	// -> 012345を出力
+	}
+}
