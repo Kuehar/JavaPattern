a4b5dca56ff32e1c5a60444e2a984ccdb45c0784
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-09 22:06:49 +0900

    Update competitiveProgramming.java

--- competitiveProgramming.java
+++ competitiveProgramming.java
@@ -79,7 +79,32 @@ public class competitiveProgramming {
 		}
 		return true;
 	}
-
+	
+	/*
+	 * 文字列内に存在する空白文字を"%20"で置き換えるメソッド
+	 */
+	
+	public void replaceSpaces(char[] str,int length) {
+		int spaceCount = 0,newLength = 0;
+		for(int i=0;i<length;i++) {
+			if(str[i] == ' ') {
+				spaceCount++;
+			}
+		}
+		newLength = length + spaceCount * 2;
+		str[newLength] = '0';
+		for(int i=length-1;i>=0;i--) {
+			if(str[i] == ' ') {
+			str[newLength-1] = '0';
+			str[newLength-2] = '2';
+			str[newLength-3] = '%';
+			newLength = newLength-3;
+		}else {
+			str[newLength-1] = str[i];
+			newLength = newLength-1;
+		}
+	}	
+	}
 	
 
 }
