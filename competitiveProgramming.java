c5577b867de849a204af60089477d31393b974b9
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-09 14:29:39 +0900

    Update competitiveProgramming.java

--- competitiveProgramming.java
+++ competitiveProgramming.java
@@ -56,6 +56,30 @@ public class competitiveProgramming {
 			return false;
 		}
 		return sort(s).equals(sort(t));
-	}	
+	}
+	
+	public boolean permutation2(String s,String t) {
+		if(s.length() != t.length()) {
+			return false;
+		}
+		
+		
+		int[] letters = new int[256];
+		
+		char[] s_array = s.toCharArray();
+		for(char c: s_array) {
+			letters[c]++;
+		}
+
+		for(int i=0;i<t.length();i++) {
+			int c = (int) t.charAt(i);
+			if(--letters[c] < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	
 
 }
