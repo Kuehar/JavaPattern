6cba67bd87ca60892b7ea795e686a7d848737a76
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-12 00:21:42 +0900

    Update competitiveProgramming.java

--- competitiveProgramming.java
+++ competitiveProgramming.java
@@ -105,6 +105,24 @@ public class competitiveProgramming {
 		}
 	}	
 	}
-	
+
+	/*
+	 * 文字列圧縮アルゴリズム(微妙なやつ)
+	 */
+	public String compressBad(String str) {
+		String myStr = "";
+		char last = str.charAt(0);
+		int count = 1;
+		for(int i=1;i < str.length();i++) {
+			if(str.charAt(i) == last) {
+				count++;
+			}else{
+				myStr += last + "" + count;
+				last = str.charAt(i);
+				count = 1;
+			}
+		}
+		return myStr + last + count;
+	}
 
 }
