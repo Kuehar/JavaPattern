8e344eda6531b16e9bf5d7a763c6d496d5e238ad
Kuehar <50407759+Kuehar@users.noreply.github.com>
2021-12-12 00:22:23 +0900

    Update competitiveProgramming.java

--- competitiveProgramming.java
+++ competitiveProgramming.java
@@ -152,5 +152,19 @@ public class competitiveProgramming {
 		myStr.append(count);
 		return myStr.toString(); 
 	}
+	int countCompression(String str) {
+		char last = str.charAt(0);
+		int size = 0;
+		int count = 1;
+		for(int i=1;i<str.length();i++) {
+			if(str.charAt(i) == last){
+				last = str.charAt(i);
+				size += 1 + String.valueOf(count).length();
+				count += 1;
+			}
+		}
+		size += 1 + String.valueOf(count).length();
+		return size;
+	}
 
 }
